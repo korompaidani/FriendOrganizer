4e1130c0846939eb0905ab668f30093adb3de2df
korompaidani <korompaidani@gmail.com>
2020-01-12 16:04:04 +0100

    After db migration and first seed

---
+++ FrienOrganizer.DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,27 @@
+﻿namespace FrienOrganizer.DataAccess.Migrations
+{
+    using FriendOrganizer.Model;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FrienOrganizer.DataAccess.FriendOrganizerDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FrienOrganizer.DataAccess.FriendOrganizerDbContext context)
+        {
+            context.Friends.AddOrUpdate(
+                f => f.FirstName,
+                new Friend { FirstName = "Daniel", LastName = "Korompai" },
+                new Friend { FirstName = "Ramona", LastName = "Donko" },
+                new Friend { FirstName = "Judit", LastName = "Szabo" },
+                new Friend { FirstName = "Miklos", LastName = "Korompai" }
+                );
+        }
+    }
+}
