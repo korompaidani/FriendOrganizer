b5cda7f43f483ed1d6e585c7159c42a3181a51af
korompaidani <korompaidani@gmail.com>
2020-01-12 15:36:30 +0100

    Comments about callerName pass OnPropertyChanged event

--- FriendOrganizer.UI/ViewModel/MainViewModel.cs
+++ FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -9,6 +9,12 @@ namespace FriendOrganizer.UI.ViewModel
         private IFriendDataService _friendDataService;
         private Friend _selectedFriend;
 
+        /// <summary>
+        /// We have know the name of changed property
+        /// e.g. OnPropertyChanged("SelectedFriend");
+        /// e.g. OnPropertyChanged(nameof(SelectedFriend));
+        /// e.g. OnPropertyChanged() --> [CallerMemberName]string propertyName = null
+        /// </summary>
         public Friend SelectedFriend
         {
             get
--- FriendOrganizer.UI/ViewModel/ViewModelBase.cs
+++ FriendOrganizer.UI/ViewModel/ViewModelBase.cs
@@ -7,6 +7,11 @@ namespace FriendOrganizer.UI.ViewModel
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
+        /// <summary>
+        /// We have know the name of changed property
+        /// e.g. string propertyName
+        /// </summary>
+        /// <param name="propertyName"></param>
         protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
